db0eaad759a781f1ee9d2d6593d97ccd4cc0c773
luuman <512769206@qq.com>
2018-03-09 14:03:19 +0800

    having

--- src/views/basis/info.vue
+++ src/views/basis/info.vue
@@ -5,48 +5,47 @@
       <div class="th">
         <div class="box1">分公司</div>
         <div class="box2">营业部</div>
-        <div class="box3">营业额-{{date - 1}}(元)</div>
-        <div class="box4">营业额-{{date}}(元)</div>
-        <div class="box5">返佣-{{date - 1}}(元)</div>
-        <div class="box6">返佣-{{date}}(元)</div>
-        <div class="box7">出游人数-{{date - 1}}</div>
-        <div class="box8">出游人-{{date}}</div>
+        <div class="box3">营业额-{{date}}/{{months}}(元)</div>
+        <div class="box3">同比(营业额)(%)</div>
+        <div class="box3">环比(营业额)(%)</div>
+        <div class="box3">返佣-{{date}}/{{months}}(元)</div>
+        <div class="box3">同比(返佣)(%)</div>
+        <div class="box3">环比(返佣)(%)</div>
+        <div class="box3">出游人数-{{date}}/{{months}}</div>
+        <div class="box3">同比(人数)(%)</div>
+        <div class="box3">环比(人数)(%)</div>
       </div>
       <div v-if="!showNo" :class="{td: true, num: item.companyName === '汇总'}" v-for="(item, index) in cityList">
         <div class="box" v-if="!item.show">
           <div class="box1" @click="show(item.companyName, index)"><i class="add"></i>{{item.companyName}}</div>
           <div class="box2"></div>
-          <div class="box3">{{item.turnovers | format}}</div>
-          <div class="box4">{{item.turnover | format}}</div>
-          <div class="box5">{{item.rebates | format}}</div>
-          <div class="box6">{{item.rebate | format}}</div>
-          <div class="box7">{{item.travelnums | format}}</div>
-          <div class="box8">{{item.travelnum | format}}</div>
+          <div class="box3">{{item.turnover | format}}</div>
+          <div class="box3">{{item.turnoverYearonyear | formatFixed}}<i :class="{up: ups(item.turnoverYearonyear)}"></i></div>
+          <div class="box3">{{item.turnoverMonthonmonth | formatFixed}}<i :class="{up: ups(item.turnoverMonthonmonth)}"></i></div>
+          <div class="box3">{{item.rebate | format}}</div>
+          <div class="box3">{{item.rebateYearonyear | formatFixed}}<i :class="{up: ups(item.rebateYearonyear)}"></i></div>
+          <div class="box3">{{item.rebateMonthonmonth | formatFixed}}<i :class="{up: ups(item.rebateMonthonmonth)}"></i></div>
+          <div class="box3">{{item.travelnum | format}}</div>
+          <div class="box3">{{item.travelnumYearonyear | formatFixed}}<i :class="{up: ups(item.travelnumYearonyear)}"></i></div>
+          <div class="box3">{{item.travelnumMonthonmonth | formatFixed}}<i :class="{up: ups(item.travelnumMonthonmonth)}"></i></div>
         </div>
         <div class="boxz" v-if="item.show">
           <div class="box1" @click="show(item.companyName, index)">box<span><i class="subtract"></i>{{item.companyName}}</span></div>
           <div class="boxs">
             <div :class="{num: items.branchName === '汇总'}" v-for="items in item.list">
               <div class="box2">{{items.branchName}}</div>
-              <div class="box3">{{items.turnovers | format}}</div>
-              <div class="box4">{{items.turnover | format}}</div>
-              <div class="box5">{{items.rebates | format}}</div>
-              <div class="box6">{{items.rebate | format}}</div>
-              <div class="box7">{{items.travelnums | format}}</div>
-              <div class="box8">{{items.travelnum | format}}</div>
+              <div class="box3">{{items.turnover | format}}</div>
+              <div class="box3">{{items.turnoverYearonyear | formatFixed}}<i :class="{up: ups(items.turnoverYearonyear)}"></i></div>
+              <div class="box3">{{items.turnoverMonthonmonth | formatFixed}}<i :class="{up: ups(items.turnoverMonthonmonth)}"></i></div>
+              <div class="box3">{{items.rebate | format}}</div>
+              <div class="box3">{{items.rebateYearonyear | formatFixed}}<i :class="{up: ups(items.rebateYearonyear)}"></i></div>
+              <div class="box3">{{items.rebateMonthonmonth | formatFixed}}<i :class="{up: ups(items.rebateMonthonmonth)}"></i></div>
+              <div class="box3">{{items.travelnum | format}}</div>
+              <div class="box3">{{items.travelnumYearonyear | formatFixed}}<i :class="{up: ups(items.travelnumYearonyear)}"></i></div>
+              <div class="box3">{{items.travelnumMonthonmonth | formatFixed}}<i :class="{up: ups(items.travelnumMonthonmonth)}"></i></div>
             </div>
           </div>
         </div>
-      </div>
-      <div v-if="showNo" class="td" v-for="item in cityLists">
-        <div></div>
-        <div>{{item.branchName}}</div>
-        <div>{{item.turnovers}}</div>
-        <div>{{item.turnover}}</div>
-        <div>{{item.rebates}}</div>
-        <div>{{item.rebate}}</div>
-        <div>{{item.travelnums}}</div>
-        <div>{{item.travelnum}}</div>
       </div>
     </div>
   </div>
@@ -60,10 +59,20 @@
     },
     filters: {
       format (num) {
+        // console.log(num)
         return Number(num).toFixed().replace(/\B(?=(\d{3})+\b)/g, ',').replace(/^/, ' ')
+      },
+      formatFixed (num) {
+        if (num === null) return 0
+        return Number(String(num).replace('%', '')).toFixed() + '%'
+        // return Number((num).replace('%', '')).toFixed()
       }
     },
     methods: {
+      ups (data) {
+        if (!data) return true
+        return (data.replace('%', '') > 0)
+      },
       show (name, index) {
         if (name === '汇总') return
         let citys = this.cityList[index]
@@ -71,46 +80,34 @@
           this.cityList[index].show = false
         } else {
           citys.show = true
-          API.queryRevenue('turnover', this.date, '', this.$route.params.city, name).then(res => {
+          API.queryRevenue('rate', this.date, this.months, this.$route.params.city, name).then(res => {
             if (res.flag === 20000) {
-              API.queryRevenue('turnover', this.date - 1, '', this.$route.params.city, name).then(ress => {
-                if (ress.flag === 20000) {
-                  res.data.companyRevenue.parmList.forEach((v) => {
-                    let citys = {
-                      turnover: 0,
-                      rebate: 0,
-                      travelnum: 0
-                    }
-                    ress.data.companyRevenue.parmList.forEach((l) => {
-                      if (v.branchName === l.branchName) {
-                        citys.turnover = l.turnover
-                        citys.rebate = l.rebate
-                        citys.travelnum = l.travelnum
-                      }
-                    })
-                    this.cityList[index].list.push({
-                      branchName: v.branchName,
-                      turnover: v.turnover,
-                      turnovers: citys.turnover,
-                      rebate: v.rebate,
-                      rebates: citys.rebate,
-                      travelnums: citys.travelnum,
-                      travelnum: v.travelnum
-                    })
-                  })
-                  console.log(this.cityList[index].list)
-                  this.cityList[index].list.push({
-                    branchName: '汇总',
-                    turnover: citys.turnover,
-                    turnovers: citys.turnovers,
-                    rebate: citys.rebate,
-                    rebates: citys.rebates,
-                    travelnums: citys.travelnums,
-                    travelnum: citys.travelnum
-                  })
-                }
-              }, (errs) => {
-                console.log(errs)
+              res.data.companyRevenue.parmList.forEach((v) => {
+                this.cityList[index].list.push({
+                  branchName: v.branchName,
+                  turnover: v.turnover,
+                  turnoverYearonyear: v.turnoverYearonyear,
+                  turnoverMonthonmonth: v.turnoverMonthonmonth,
+                  rebate: v.rebate,
+                  rebateYearonyear: v.rebateYearonyear,
+                  rebateMonthonmonth: v.rebateMonthonmonth,
+                  travelnum: v.travelnum,
+                  travelnumYearonyear: v.travelnumYearonyear,
+                  travelnumMonthonmonth: v.travelnumMonthonmonth
+                })
+              })
+              console.log(this.cityList[index].list)
+              this.cityList[index].list.push({
+                branchName: '汇总',
+                turnover: citys.turnover,
+                turnoverYearonyear: citys.turnoverYearonyear,
+                turnoverMonthonmonth: citys.turnoverMonthonmonth,
+                rebate: citys.rebate,
+                rebateYearonyear: citys.rebateYearonyear,
+                rebateMonthonmonth: citys.rebateMonthonmonth,
+                travelnum: citys.travelnum,
+                travelnumYearonyear: citys.travelnumYearonyear,
+                travelnumMonthonmonth: citys.travelnumMonthonmonth
               })
             }
           }, (err) => {
@@ -124,58 +121,42 @@
       }
     },
     mounted () {
-      API.queryRevenue('turnover', this.date, '', this.$route.params.city, '').then(res => {
+      API.queryRevenue('rate', this.date, this.months, this.$route.params.city, '').then(res => {
         if (res.flag === 20000) {
-          API.queryRevenue('turnover', this.date - 1, '', this.$route.params.city, '').then(ress => {
-            if (ress.flag === 20000) {
-              let last = {
-                companyName: '汇总',
-                turnover: 0,
-                turnovers: 0,
-                rebate: 0,
-                rebates: 0,
-                travelnums: 0,
-                travelnum: 0,
-                show: false,
-                list: []
-              }
-              res.data.companyRevenue.parmList.forEach((v) => {
-                let citys = {
-                  turnover: 0,
-                  rebate: 0,
-                  travelnum: 0
-                }
-                ress.data.companyRevenue.parmList.forEach((l) => {
-                  if (v.companyName === l.companyName) {
-                    citys.turnover = l.turnover
-                    citys.rebate = l.rebate
-                    citys.travelnum = l.travelnum
-                  }
-                })
-                console.log(Number(citys.turnover))
-                last.turnover += Number(v.turnover)
-                last.turnovers += Number(citys.turnover)
-                last.rebate += Number(v.rebate)
-                last.rebates += Number(citys.rebate)
-                last.travelnums += Number(citys.travelnum)
-                last.travelnum += Number(v.travelnum)
-                this.cityList.push({
-                  companyName: v.companyName,
-                  turnover: v.turnover,
-                  turnovers: citys.turnover,
-                  rebate: v.rebate,
-                  rebates: citys.rebate,
-                  travelnums: citys.travelnum,
-                  travelnum: v.travelnum,
-                  show: false,
-                  list: []
-                })
-              })
-              this.cityList.push(last)
-            }
-          }, (errs) => {
-            console.log(errs)
+          let last = {
+            companyName: '汇总',
+            show: false,
+            turnover: 0,
+            turnoverYearonyear: 0,
+            turnoverMonthonmonth: 0,
+            rebate: 0,
+            rebateYearonyear: 0,
+            rebateMonthonmonth: 0,
+            travelnum: 0,
+            travelnumYearonyear: 0,
+            travelnumMonthonmonth: 0,
+            list: []
+          }
+          res.data.companyRevenue.parmList.forEach((v) => {
+            last.turnover += Number(v.turnover)
+            last.rebate += Number(v.rebate)
+            last.travelnum += Number(v.travelnum)
+            this.cityList.push({
+              companyName: v.companyName,
+              turnover: v.turnover,
+              turnoverYearonyear: v.turnoverYearonyear,
+              turnoverMonthonmonth: v.turnoverMonthonmonth,
+              rebate: v.rebate,
+              rebateYearonyear: v.rebateYearonyear,
+              rebateMonthonmonth: v.rebateMonthonmonth,
+              travelnum: v.travelnum,
+              travelnumYearonyear: v.travelnumYearonyear,
+              travelnumMonthonmonth: v.travelnumMonthonmonth,
+              show: false,
+              list: []
+            })
           })
+          this.cityList.push(last)
         }
       }, (err) => {
         console.log(err)
@@ -187,7 +168,9 @@
         showNo: false,
         cityList: [],
         cityLists: [],
-        date: new Date().getFullYear()
+        date: new Date().getFullYear(),
+        // months: new Date().getMonth()
+        months: '01'
       }
     }
   }
@@ -210,14 +193,14 @@
       div{
         color: #666;
         border-bottom: solid size(1) #EEE;
-        &:nth-child(8n+3), &:nth-child(8n+4), &:nth-child(8n+5), &:nth-child(8n+6), &:nth-child(8n+7), &:nth-child(8n+8){
+        &.box3,&.box2{
           border-right: solid size(1) #EEE;
           text-align: right;
         }
       }
     }
     .boxz{
-      width: size(1120);
+      width: size(1480);
       overflow: hidden;
       position: relative;
       // float: left;
@@ -254,7 +237,7 @@
         }
       }
       .boxs{
-        width: size(920);
+        width: size(1280);
         .num{
           background: rgba(255, 235, 63, 0.15);
         }
@@ -279,7 +262,7 @@
       }
     }
     .th,.td .box{
-      width: size(1120);
+      width: size(1480);
       height: size(40);
       div{
         float: left;
@@ -300,7 +283,7 @@
       white-space: nowrap;
       text-overflow: ellipsis;
     }
-    .box3, .box4, .box5, .box6, .box7, .box8{
+    .box3{
       width: size(120);
       overflow: hidden;
       white-space: nowrap;
@@ -327,6 +310,23 @@
     }
     .add{
       background-image: url(../../assets/img/add.png);
+    }
+    .box3{
+      i{
+        width: size(18);
+        height: size(12);
+        display: inline-block;
+        vertical-align: middle;
+        line-height: size(20);
+        margin-left: size(6);
+        background-image: url(../../assets/img/base/down.png);
+        background-repeat: no-repeat;
+        background-size: 40%;
+        background-position: center;
+        &.up{
+          background-image: url(../../assets/img/base/up.png);
+        }
+      }
     }
   }
 </style>
